38432eaad5ff06a76dcfa8d89130e1787a153281
Ahmed M. Farrag <afarrag@users.noreply.github.com>
2024-09-04 02:11:52 +0000

    adding slack notification

--- app.py
+++ app.py
@@ -51,11 +51,17 @@ def process_uploaded_file(uploaded_file, participant_name):
             if (participant_results.iloc[0,1]) > (all_data.accuracy.max()):
                 autoplay_audio('static/claps.mp3')
                 rain("great.png",math.floor(random.random()*100))
+                
             else:
                 autoplay_audio('static/success.mp3')
                 rain("good.png",math.floor(random.random()*100))
             display_participant_results(participant_results)
             update_and_plot_submissions(participant_results, participant_name)
+
+            if (participant_results.iloc[0,1]) > (all_data.accuracy.max()):
+                with st.status("Sending to Slack..."):
+                    send_msg_to_slack(participant_results.iloc[0,0])
+                st.success("Sent to Slack")
         except Exception as e:
             st.error(f'The file could not be processed. Error: {e}')
             st.exception(e)
@@ -118,6 +124,72 @@ def rain(photo,x):
     my_html = f'<style>{my_css}</style><img class="rainPhoto" src="./app/static/{photo}"/>'
     st.write(my_html,unsafe_allow_html=True)
     
+def send_msg_to_slack(new_top):
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    import time
+    import os
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
+    ########################################################
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--log-level=3')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-infobars')
+
+    # Slack credentials and channel URL
+    SLACK_URL = os.getenv("SLACK_URL")
+    EMAIL = os.getenv("EMAIL")
+    PASSWORD = os.getenv("PASSWORD")
+    CHANNEL_URL = os.getenv("CHANNEL_URL")
+   
+    # Initialize WebDriver (here using Chrome)
+    driver = webdriver.Chrome(options=options, service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ))
+    driver.get(SLACK_URL)
+    time.sleep(2)  # Adjust if necessary to allow time for page load
+
+    from urllib.request import urlopen
+    import pickle
+
+    cookies = urlopen(os.getenv("cookies"))
+    cookies = pickle.load(cookies)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    # Open Slack and login
+    
+
+    # Find and fill in email and password fields, then click login
+    from selenium.webdriver.common.by import By
+
+    email_input=driver.find_element(By.ID, 'email')
+    email_input.send_keys(EMAIL)
+    password_input=driver.find_element(By.ID, 'password')
+    password_input.send_keys(PASSWORD)
+    password_input.send_keys(Keys.RETURN)
+    time.sleep(5)  # Adjust if necessary to allow time for page load
+
+
+    # Open Slack and login
+    driver.get(CHANNEL_URL)
+    time.sleep(2)  # Adjust if necessary to allow time for page load
+
+    msg = f":rocket: :trophy: {new_top} is now #1 on the [LEADERBORD](https://datathon.streamlit.app)!"
+
+    message_box = driver.find_element(By.CLASS_NAME, 'ql-editor')
+    message_box.send_keys(msg)
+    message_box.send_keys(Keys.RETURN)
+
+
+
 
 if __name__ == "__main__":
     main()
